03ebce287015032a03092cd61636cd2547993c6d
Hugo Yanez <hugodavidyanez@gmail.com>
2021-12-08 21:26:24 -0500

    Created workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+const Scheme = mongoose.Schema;
+
+const WorkoutSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+  exercise: [
+    {
+      type: Schema.Types.ObjectID,
+      ref: "Exercise",
+    },
+  ],
+});
