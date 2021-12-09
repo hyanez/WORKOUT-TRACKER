f0b1127a4706ca07df3981b28ff7c8f10cbc60bc
Hugo Yanez <hugodavidyanez@gmail.com>
2021-12-08 21:01:26 -0500

    Created mongoose connection

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 const databaseURL = "workout";
 const collections = ["exercises"];
+const workoutDB = mongojs(databaseUrl, collections);
 
 app.use(logger("dev"));
 
@@ -16,6 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
