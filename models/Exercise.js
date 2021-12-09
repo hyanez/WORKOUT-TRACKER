6e2b2dc1a97e53d29398329c64a56fd11430c5b6
Hugo Yanez <hugodavidyanez@gmail.com>
2021-12-08 21:37:43 -0500

    Created exercise schema

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,1 +1,13 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  name: String,
+  type: String,
+  weight: String,
+  sets: String,
+  reps: String,
+  duration: String,
+  distance: String,
+});
